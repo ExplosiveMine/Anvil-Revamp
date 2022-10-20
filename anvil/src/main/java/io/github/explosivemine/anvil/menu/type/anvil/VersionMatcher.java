16f6bdf7258b59dd0edcef2c19ccbaac64bba361
ExplosiveMine <patel.muha@gmail.com>
2022-10-20 17:39:04 +0400

    v1.3.3

--- anvil/src/main/java/io/github/explosivemine/anvil/menu/type/anvil/VersionMatcher.java
+++ anvil/src/main/java/io/github/explosivemine/anvil/menu/type/anvil/VersionMatcher.java
@@ -1,23 +1,29 @@
 package io.github.explosivemine.anvil.menu.type.anvil;
 
-import io.github.explosivemine.anvil.version.VersionWrapper;
+import io.github.explosivemine.anvil.version.*;
 import org.bukkit.Bukkit;
 
-public final class VersionMatcher {
-    public VersionWrapper match() {
-        final String serverVersion = Bukkit.getServer()
-                .getClass()
-                .getPackage()
-                .getName()
-                .split("\\.")[3]
-                .substring(1);
+import java.util.HashMap;
+import java.util.Map;
 
+public final class VersionMatcher {
+    private final static Map<String, Class<? extends VersionWrapper>> map = new HashMap<>() {{
+        put("1.17-R0.1-SNAPSHOT", Wrapper1_17_R1.class);
+        put("1.17.1-R0.1-SNAPSHOT", Wrapper1_17_R1.class);
+        put("1.18-R0.1-SNAPSHOT", Wrapper1_18_R1.class);
+        put("1.18.1-R0.1-SNAPSHOT", Wrapper1_18_R1.class);
+        put("1.18.2-R0.1-SNAPSHOT", Wrapper1_18_R2.class);
+        put("1.19-R0.1-SNAPSHOT", Wrapper1_19_R1.class);
+        put("1.19.1-R0.1-SNAPSHOT", Wrapper1_19_1_R1.class);
+        put("1.19.2-R0.1-SNAPSHOT", Wrapper1_19_1_R1.class);
+    }};
+
+    public VersionWrapper match() {
+        String bukkitVersion = Bukkit.getBukkitVersion();
         try {
-            return (VersionWrapper) Class.forName("io.github.explosivemine.anvil.version.Wrapper" + serverVersion).newInstance();
+            return map.get(bukkitVersion).newInstance();
         } catch (IllegalAccessException | InstantiationException exception) {
-            throw new IllegalStateException("Failed to instantiate version wrapper for version " + serverVersion, exception);
-        } catch (ClassNotFoundException exception) {
-            throw new IllegalStateException("Anvil does not support server version \"" + serverVersion + "\"", exception);
+            throw new IllegalStateException("Failed to instantiate version wrapper for version " + bukkitVersion, exception);
         }
     }
 
